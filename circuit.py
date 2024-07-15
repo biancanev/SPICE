cbbee423d8f676d4c65b5c91fc829ac08cb8b1db
biancanev <bebostunner234@gmail.com>
2024-07-14 22:09:02 -0700

    Changed to python

---
+++ circuit.py
@@ -0,0 +1,124 @@
+import numpy as np
+
+WF_DC = 0
+WF_AC = 1
+WF_SQUARE = 2
+
+class CircuitElement:
+    def __init__(self, x:int, y:int, w:int, h:int, file_name:str, index:int):
+        self.x, self.y, self.w, self.h, self.index= x, y, w, h, index
+        self.file_name = file_name
+        self.voltage, self.current = 0, 0
+
+    def simulate(self) -> tuple[int, int]:
+        return (self.voltage, self.current)
+
+class Node:
+    def __init__(self, index):
+        self.index = index
+        self.elements = list[CircuitElement]
+        self.voltage = int()
+
+class TwoPinElement(CircuitElement):
+    def __init__(self, x:int, y:int, w:int, h:int, file_name:str, index: int):
+        super().__init__(x, y, w, h, file_name, index)
+        self.topConnection = Node("")
+        self.bottomConnection = Node("")
+    
+    def connectToNode(self, pin:int, node:Node):
+        if pin == 0:
+            self.topConnection = node
+        elif pin == 1:
+            self.bottomConnection = node
+        else:
+            raise "Invalid pin number"
+
+
+class Ground(Node):
+    def __init__(self, index):
+        super().__init__(index)
+        self.node = 0
+
+
+class Resistor(TwoPinElement):
+    def __init__(self, x:int, y:int, w:int, h:int, index:int, res:float=0, tol:float=0):
+        super().__init__(x, y, w, h, "", index)
+        self.resistance, self.tolerance = res, tol
+
+    def simulate(self) -> tuple[int, int]:
+        self.voltage = self.topConnection.voltage - self.bottomConnection.voltage
+        self.current = self.voltage / self.resistance
+        return (self.voltage, self.current)
+    
+    def getName(self) -> str:
+        return "R" + str(self.index)
+    
+class VoltageSource(TwoPinElement):
+    def __init__(self, x:int, y:int, w:int, h:int, wf:int, index:int, v:int):
+        match wf:
+            case 0:
+                file_name = "elements/VoltageSource.svg.png"
+                
+            case 1:
+                file_name = ""
+            case _:
+                file_name = "elements/VoltageSource.svg.png"
+        super().__init__(x, y, w, h, file_name, index)
+        self.voltage = v
+
+    def simulate(self) -> tuple[int, int]:
+        self.current = 0
+        self.topConnection.voltage = self.bottomConnection.voltage + self.voltage
+        print(self.voltage, self.topConnection.voltage)
+        return self.voltage, self.current
+    def getName(self) -> str:
+        return "V" + str(self.index)
+
+class Circuit:
+    def __init__(self):
+        self.elements = []
+        self.nodes = []
+
+    def addElement(self, element:CircuitElement):
+        self.elements.append(element)
+
+    def addNode(self, node):
+        self.nodes.append(node)
+
+    #Note: pin = 0 is +, pin = 1 is -
+    def connectElementtoNode(self, elementIndex, nodeIndex, pin):
+        if pin == 0:
+            self.elements[elementIndex].topConnection = self.nodes[nodeIndex]
+        elif pin == 1:
+            self.elements[elementIndex].bottomConnection = self.nodes[nodeIndex]
+        
+        #self.nodes[nodeIndex].elements.append(self.elements[elementIndex])
+    
+    def simulate(self):
+        for element in self.elements:
+            res = element.simulate()
+            v, i = res[0], res[1]
+            print("Element: {0}, V = {1}, I = {2}".format(element.getName(), v, i))
+
+
+
+#Just for testing...
+circuit = Circuit()
+
+v1 = VoltageSource(0, 0, 2, 4, WF_DC, 1, 5)
+circuit.addElement(v1)
+r1 = Resistor(4, 0, 2, 4, 1, 10)
+circuit.addElement(r1)
+u1 = Node("u1")
+circuit.addNode(u1)
+gnd = Ground("gnd")
+circuit.addNode(gnd)
+circuit.connectElementtoNode(circuit.elements.index(v1), circuit.nodes.index(u1), 0)
+circuit.connectElementtoNode(circuit.elements.index(v1), circuit.nodes.index(gnd), 1)
+circuit.connectElementtoNode(circuit.elements.index(r1), circuit.nodes.index(u1), 0)
+circuit.connectElementtoNode(circuit.elements.index(r1), circuit.nodes.index(gnd), 1)
+
+circuit.simulate()
+
+
+
---
+++ gui.py
@@ -0,0 +1,121 @@
+import sys
+from circuit import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import  *
+
+
+class MovingObject(QGraphicsEllipseItem):
+    def __init__(self, x, y, r):
+        super().__init__(0, 0, r, r)
+        self.setPos(x, y)
+        self.setBrush(Qt.blue)
+        self.setAcceptHoverEvents(True)
+
+    # mouse hover event
+    def hoverEnterEvent(self, event):
+        app.instance().setOverrideCursor(Qt.OpenHandCursor)
+
+    def hoverLeaveEvent(self, event):
+        app.instance().restoreOverrideCursor()
+
+    # mouse click event
+    def mousePressEvent(self, event):
+        pass
+
+    def mouseMoveEvent(self, event):
+        orig_cursor_position = event.lastScenePos()
+        updated_cursor_position = event.scenePos()
+
+        orig_position = self.scenePos()
+
+        updated_cursor_x = updated_cursor_position.x() - orig_cursor_position.x() + orig_position.x()
+        updated_cursor_y = updated_cursor_position.y() - orig_cursor_position.y() + orig_position.y()
+        self.setPos(QPointF(updated_cursor_x, updated_cursor_y))
+
+    def mouseReleaseEvent(self, event):
+        print('x: {0}, y: {1}'.format(self.pos().x(), self.pos().y()))
+
+class PreviewObject(QGraphicsPixmapItem):
+    def __init__(self, x, y, fileName):
+        self.pm = QPixmap(fileName)
+        self.pm = self.pm.scaled(50, 50, Qt.KeepAspectRatio)
+        super().__init__(self.pm)
+        self.setPos(x, y)
+        self.setAcceptHoverEvents(True)
+
+    def hoverEnterEvent(self, event):
+        app.instance().setOverrideCursor(Qt.OpenHandCursor)
+
+    def hoverLeaveEvent(self, event):
+        app.instance().restoreOverrideCursor()
+    def mouseMoveEvent(self, event):
+        orig_cursor_position = event.lastScenePos()
+        updated_cursor_position = event.scenePos()
+
+        orig_position = self.scenePos()
+
+        updated_cursor_x = updated_cursor_position.x() - orig_cursor_position.x() + orig_position.x()
+        updated_cursor_y = updated_cursor_position.y() - orig_cursor_position.y() + orig_position.y()
+        self.setPos(updated_cursor_x, updated_cursor_y)
+        print('x: {0}, y: {1}'.format(updated_cursor_position.x(), updated_cursor_position.y()))
+
+class GraphicView(QGraphicsView):
+    def __init__(self, a):
+        super().__init__(a)
+
+        self.scene = QGraphicsScene()
+        self.setScene(self.scene)       
+        self.setSceneRect(0, 0, 1080, 720)
+
+        self.moveObject = MovingObject(50, 50, 40)
+        self.previewObject = PreviewObject(20, 20, "elements\Voltage_Source.svg.png")
+        # self.moveObject2 = MovingObject(100, 100, 100)
+        self.scene.addItem(self.moveObject)
+        self.scene.addItem(self.previewObject)
+        # self.scene.addItem(self.moveObject2)
+
+
+class MainView(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.detectedErrors = 0
+
+        self.setFixedSize(QSize(1080, 720))
+        self.setWindowTitle("SPICE Simulator [ver xx.xx.xx]")
+        self.centralwidget = QWidget()
+        self.setCentralWidget(self.centralwidget)
+
+        pixmapi = getattr(QStyle, "SP_MediaPlay")
+        icon = self.style().standardIcon(pixmapi)
+        startButton = QAction(QIcon(icon), "Start", self)
+        sourceButton = QAction(QIcon("elements\Voltage_Source.svg.png"), "Source", self)
+        pixmapi = getattr(QStyle, "SP_FileIcon")
+        icon = self.style().standardIcon(pixmapi)
+        newfileButton = QAction(QIcon(icon), "&New Schematic", self)
+        
+
+        taskbar = QToolBar("main")
+        self.addToolBar(taskbar)
+        taskbar.addAction(startButton)
+        taskbar.addAction(sourceButton)
+
+        menu = self.menuBar()
+
+        file_menu = menu.addMenu("&File")
+        file_menu.addAction(newfileButton)
+
+        simulation_menu = menu.addMenu("&Simulation")
+        
+
+        self.mainview = GraphicView(self.centralwidget)
+
+        self.setStatusBar(QStatusBar(self))
+
+
+app = QApplication(sys.argv)
+view = MainView()
+view.show()
+sys.exit(app.exec_())
+
