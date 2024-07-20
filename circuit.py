af9b763cf995eb141bc405c40a689238ac374fd2
biancanev <bebostunner234@gmail.com>
2024-07-20 11:00:53 -0700

    Fixed most of the bugs in nodal analysis. Still not 100% correct. Added unit testing.

--- circuit.py
+++ circuit.py
@@ -1,4 +1,60 @@
 import numpy as np
+import unittest
+
+#Add Tests to ensure all functionality works
+class DC_Analysis_Test(unittest.TestCase):
+    def test_ohms_law(self):
+        circuit = Circuit()
+        v1 = VoltageSource(0, 0, 2, 4, WF_DC, 0, 5)
+        circuit.addElement(v1)
+        r1 = Resistor(4, 0, 2, 4, 1, 10)
+        circuit.addElement(r1)
+        u1 = Node(0)
+        circuit.addNode(u1)
+        gnd = Ground(1)
+        circuit.addNode(gnd)
+        circuit.connectElementtoNode(circuit.elements.index(v1), circuit.nodes.index(u1), 0)
+        circuit.connectElementtoNode(circuit.elements.index(v1), circuit.nodes.index(gnd), 1)
+        circuit.connectElementtoNode(circuit.elements.index(r1), circuit.nodes.index(u1), 0)
+        circuit.connectElementtoNode(circuit.elements.index(r1), circuit.nodes.index(gnd), 1)
+
+        circuit.simulate()
+        self.assertEqual(v1.voltage, 5)
+        self.assertEqual(r1.voltage, 5)
+        self.assertEqual(r1.current, 0.5)
+    
+    def test_nodal_analysis(self):
+        circuit2 = Circuit()
+        v1 = VoltageSource(0, 0, 2, 4, WF_DC, 0, 5)
+        r1 = Resistor(4, 0, 2, 4, 1, 2)
+        circuit2.addElement(v1)
+        circuit2.addElement(r1)
+        r2 = Resistor(4, 0, 2, 4, 2, 2)
+        r3 = Resistor(4, 0, 2, 4, 3, 4)
+        circuit2.addElement(r2)
+        circuit2.addElement(r3)
+        u1 = Node(0)
+        u2 = Node(1)
+        gnd = Ground(2)
+        circuit2.addNode(u1)
+        circuit2.addNode(u2)
+        circuit2.addNode(gnd)
+        circuit2.connectElementtoNode(circuit2.elements.index(v1), circuit2.nodes.index(u1), 0)
+        circuit2.connectElementtoNode(circuit2.elements.index(v1), circuit2.nodes.index(gnd), 1)
+        circuit2.connectElementtoNode(circuit2.elements.index(r1), circuit2.nodes.index(u1), 0)
+        circuit2.connectElementtoNode(circuit2.elements.index(r1), circuit2.nodes.index(u2), 1)
+        circuit2.connectElementtoNode(circuit2.elements.index(r2), circuit2.nodes.index(u2), 0)
+        circuit2.connectElementtoNode(circuit2.elements.index(r2), circuit2.nodes.index(gnd), 1)
+        circuit2.connectElementtoNode(circuit2.elements.index(r3), circuit2.nodes.index(u2), 0)
+        circuit2.connectElementtoNode(circuit2.elements.index(r3), circuit2.nodes.index(gnd), 1)
+
+        circuit2.simulate()
+        self.assertEqual(abs(r1.current), (5-2/3)/2)
+        self.assertEqual(abs(r1.voltage), 5 - 2/3)
+        self.assertEqual(abs(r2.current), 2)
+        self.assertEqual(abs(r2.voltage), 4)
+        self.assertEqual(abs(r3.current), 2/3/4)
+        self.assertEqual(abs(r3.voltage), 2/3)
 
 WF_DC = 0
 WF_AC = 1
@@ -6,7 +62,7 @@ WF_SQUARE = 2
 
 class CircuitElement:
     def __init__(self, x:int, y:int, w:int, h:int, file_name:str, index:int):
-        self.x, self.y, self.w, self.h, self.index= x, y, w, h, index
+        self.x, self.y, self.w, self.h, self.index = x, y, w, h, index
         self.file_name = file_name
         self.voltage, self.current = 0, 0
 
@@ -14,13 +70,15 @@ class CircuitElement:
         return (self.voltage, self.current)
 
 class Node:
-    def __init__(self, index):
+    def __init__(self, index:int):
         self.index = index
-        self.elements = list[CircuitElement]
+        self.changed = False
+        self.elements: list[CircuitElement] = []
         self.voltage = int()
+        self.coordinates = list[tuple[int, int]]
 
 class TwoPinElement(CircuitElement):
-    def __init__(self, x:int, y:int, w:int, h:int, file_name:str, index: int):
+    def __init__(self, x:int, y:int, w:int, h:int, file_name:str, index:int):
         super().__init__(x, y, w, h, file_name, index)
         self.topConnection = Node("")
         self.bottomConnection = Node("")
@@ -35,9 +93,10 @@ class TwoPinElement(CircuitElement):
 
 
 class Ground(Node):
-    def __init__(self, index):
+    def __init__(self, index:int):
         super().__init__(index)
         self.node = 0
+        self.changed = True
 
 
 class Resistor(TwoPinElement):
@@ -58,7 +117,6 @@ class VoltageSource(TwoPinElement):
         match wf:
             case 0:
                 file_name = "elements/VoltageSource.svg.png"
-                
             case 1:
                 file_name = ""
             case _:
@@ -69,6 +127,7 @@ class VoltageSource(TwoPinElement):
     def simulate(self) -> tuple[int, int]:
         self.current = 0
         self.topConnection.voltage = self.bottomConnection.voltage + self.voltage
+        self.topConnection.changed = True
         return self.voltage, self.current
     def getName(self) -> str:
         return "V" + str(self.index)
@@ -90,34 +149,45 @@ class Circuit:
             self.elements[elementIndex].topConnection = self.nodes[nodeIndex]
         elif pin == 1:
             self.elements[elementIndex].bottomConnection = self.nodes[nodeIndex]
-        
-        #self.nodes[nodeIndex].elements.append(self.elements[elementIndex])
+        self.nodes[nodeIndex].elements.append(self.elements[elementIndex])
     
     def simulate(self):
+        #find all known nodes
+        A = np.zeros((len(self.nodes), len(self.nodes)))
+        b = np.zeros((len(self.nodes), 1))
+        #use KCL equation for each node and substitute into the matrix
+        row = 0
+        for node in self.nodes:
+            col = 0
+            print(node.elements)
+            for element in node.elements:
+                print(row, col, ":", node, element)
+                if type(element) == VoltageSource:
+                    print("VS")
+                    A[row][element.index] += element.voltage
+                    b[row][0] += element.voltage
+                elif type(element) == Resistor:
+                    print("R")
+                    A[row][node.index] += 1 / element.resistance
+                col += 1
+            row += 1
+        print(A)
+        print(b)
+        #solve the matrix
+        x = np.linalg.solve(A, b)
+        #substitute values into corresponding nodes
+        print(x)
+        print(self.nodes)
+        for nodeIndex in range(len(A)):
+            self.nodes[nodeIndex].voltage = x[nodeIndex][0]
+        #solve for unknown element voltages and currents
+        self.outputValues()
+
+    def outputValues(self):
         for element in self.elements:
             res = element.simulate()
             v, i = res[0], res[1]
-            print("Element: {0}, V = {1}, I = {2}".format(element.getName(), v, i))
+            print("Element: {0}, V = {1}, I = {2}".format(element.getName(), v, i)) #need to change naming scheme later
 
-
-
-#Just for testing...
-circuit = Circuit()
-
-v1 = VoltageSource(0, 0, 2, 4, WF_DC, 1, 5)
-circuit.addElement(v1)
-r1 = Resistor(4, 0, 2, 4, 1, 10)
-circuit.addElement(r1)
-u1 = Node("u1")
-circuit.addNode(u1)
-gnd = Ground("gnd")
-circuit.addNode(gnd)
-circuit.connectElementtoNode(circuit.elements.index(v1), circuit.nodes.index(u1), 0)
-circuit.connectElementtoNode(circuit.elements.index(v1), circuit.nodes.index(gnd), 1)
-circuit.connectElementtoNode(circuit.elements.index(r1), circuit.nodes.index(u1), 0)
-circuit.connectElementtoNode(circuit.elements.index(r1), circuit.nodes.index(gnd), 1)
-
-circuit.simulate()
-
-
-
+if __name__ == '__main__':
+    unittest.main()
